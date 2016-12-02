e5da3b8fb254a857d0100e6fbc92395f0423279d
AG <geometric10340@gmail.com>
2016-12-03 00:21:30 +0300

    Update setting + Dockerfile

--- Container/conf/settings.py
+++ Container/conf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h3o%ys8vi=8z4ctmgx*7@+c1vx0i7&lchpx1bbwx2j9cq(+=v_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'manager',
 ]
 
 MIDDLEWARE = [
