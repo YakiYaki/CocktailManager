c4b80f128b208489bb677a9a308e770b6bb985bd
Боярских Никита Игоревич <N02@yandex.ru>
2016-12-14 02:13:32 +0300

    ВНИМАНИЕ!!! Вопрос по конфигурации django!!!

--- CocktailManager/config.py
+++ CocktailManager/config.py
@@ -4,7 +4,7 @@ class Configuration:
 	def config_get(self, section, option, defaul=None):
 		try:		
 			self.config = ConfigParser()
-			self.config.read("config.ini")	
+			self.config.read("config.ini")  #А это нормально, что у нас используется дефолтная конфигурация?????????????
 			return self.config.get(section, option)
 		except (NoSectionError, NoOptionError):
 			return default
